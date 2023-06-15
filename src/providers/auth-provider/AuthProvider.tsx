c60fe4da3ea4640fffb8204f582319869e830609
Andrey Peshko <coursestud@mail.ru>
2023-06-15 21:02:35 +0300

    feat: add auth providers

---
+++ src/providers/auth-provider/AuthProvider.tsx
@@ -0,0 +1,42 @@
+import Cookies from 'js-cookie';
+import dynamic from 'next/dynamic';
+import { useRouter } from 'next/router';
+import { FC, PropsWithChildren, useEffect } from 'react';
+
+import { useActions } from '@/hooks/useActions';
+import { useAuth } from '@/hooks/useAuth';
+
+import { TypeComponentAuthFields } from './auth-page.type';
+import { EnumNameToken } from '@/services/auth/auth.enums';
+import { getAccessToken } from '@/services/auth/auth.helper';
+
+const DynamicCheckRole = dynamic(() => import('./CheckRole'), { ssr: false });
+
+const AuthProvider: FC<PropsWithChildren<TypeComponentAuthFields>> = (
+	{ Component: { isOnlyUser } },
+	children,
+) => {
+	const { user } = useAuth();
+	const { checkAuth, logout } = useActions();
+	const { pathname } = useRouter();
+
+	useEffect(() => {
+		const accessToken = getAccessToken();
+		if (accessToken) checkAuth();
+	}, []);
+
+	useEffect(() => {
+		const refreshToken = Cookies.get(EnumNameToken.REFRESH);
+		if (!refreshToken && user) {
+			logout();
+		}
+	}, [pathname]);
+
+	return isOnlyUser ? (
+		<DynamicCheckRole Component={{ isOnlyUser }}>{children}</DynamicCheckRole>
+	) : (
+		<>{children}</>
+	);
+};
+
+export default AuthProvider;
