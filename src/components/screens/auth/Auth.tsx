12db32fb9a138cccf3ed8d3c652a97dc9151310b
Andrei <coursestud@mail.ru>
2023-06-16 15:14:07 +0300

    feat: add auth page

---
+++ src/components/screens/auth/Auth.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react';
+
+import Meta from '@/components/ui/Meta';
+import Button from '@/components/ui/button/Button';
+
+const Auth: FC = () => {
+	return (
+		<Meta title='Auth'>
+			<Button variant='orange'>Auth</Button>
+		</Meta>
+	);
+};
+
+export default Auth;
