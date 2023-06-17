cc4a17252ad781d8340211d78c98a4108080716d
Andrey Peshko <coursestud@mail.ru>
2023-06-17 14:17:03 +0300

    feat: add cart to store

--- src/hooks/useCart.ts
+++ src/hooks/useCart.ts
@@ -1,4 +1,9 @@
+import { ICartItem } from '@/types/cart.interface';
+
 import { useTypedSelector } from './useTypedSelector';
 
 // export const useCart = () => useTypedSelector(state => state.cart);
-export const useCart = { cart: [] };
+export const useCart = () => {
+	const items: ICartItem[] = [];
+	return { items };
+};
